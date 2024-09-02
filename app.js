159e85b9a5e8d07d448695d8a9434eb186a1241b
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-02 14:01:04 +0330

    feat: set helmet middleware

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
+const helmet = require("helmet");
 
 dotenv.config();
 
 const app = express();
 
 app.use(
+  helmet(),
   express.json(),
   express.urlencoded({ extended: false }),
   express.static(path.join(process.cwd(), "public"))
