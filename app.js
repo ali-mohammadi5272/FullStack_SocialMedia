87d2d23a5ee8d7b848bb206aa41e956c6fd1477f
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-02 14:00:33 +0330

    feat: set static middleware

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json(), express.urlencoded({ extended: false }));
+app.use(
+  express.json(),
+  express.urlencoded({ extended: false }),
+  express.static(path.join(process.cwd(), "public"))
+);
 
 module.exports = app;
