ddcc293d0ee4a1b9a8276580edcaa8615e041a42
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-02 14:12:31 +0330

    feat: update generateAccessToken function

--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const generateAccessToken = (payload) => {
   const { ACCESS_TOKEN_SECRET_KEY, ACCESS_TOKEN_EXPIRES_TIME } = process.env;
+
   const token = jwt.sign(payload, ACCESS_TOKEN_SECRET_KEY, {
     expiresIn: ACCESS_TOKEN_EXPIRES_TIME,
   });
