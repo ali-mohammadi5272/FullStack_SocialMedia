689e9d93c7d1589df623fbf30bed9609b0f968aa
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-02 14:01:23 +0330

    feat: set cors middleware

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
 const helmet = require("helmet");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 
 app.use(
+  cors(),
   helmet(),
   express.json(),
   express.urlencoded({ extended: false }),
