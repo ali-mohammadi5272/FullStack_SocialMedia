0f1e8d8547df74828db700d582e25853fa569419
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-02 12:48:52 +0330

    feat: add phoneNumberPrefixPattern RegEx pattern

--- utils/patterns.js
+++ utils/patterns.js
@@ -1,7 +1,9 @@
 const emailPattern = /^[\w\.\-_]+@[\w]{5,8}\.[a-z]{2,3}$/;
 const phoneNumberPattern = /^(0|\+98)?9\d{9}$/;
+const phoneNumberPrefixPattern = /^(0|\+98)/;
 
 module.exports = {
   emailPattern,
   phoneNumberPattern,
+  phoneNumberPrefixPattern,
 };
