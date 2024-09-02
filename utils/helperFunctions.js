7ee5e3a7dd3409d4c416ddf534d1ad955f1eb47c
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-02 13:41:45 +0330

    feat: create accessTokenPayload function

--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -6,13 +6,24 @@ dotenv.config();
 
 const generateAccessToken = (payload) => {
   const { ACCESS_TOKEN_SECRET_KEY, ACCESS_TOKEN_EXPIRES_TIME } = process.env;
-
   const token = jwt.sign(payload, ACCESS_TOKEN_SECRET_KEY, {
     expiresIn: ACCESS_TOKEN_EXPIRES_TIME,
   });
   return token;
 };
 
+const accessTokenPayload = (token) => {
+  const { ACCESS_TOKEN_SECRET_KEY } = process.env;
+
+  try {
+    const payload = jwt.verify(token, ACCESS_TOKEN_SECRET_KEY);
+    return payload;
+  } catch (err) {
+    return false;
+  }
+};
+
 module.exports = {
   generateAccessToken,
+  accessTokenPayload,
 };
