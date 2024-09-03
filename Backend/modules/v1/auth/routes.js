ee29068fd44c871822204ed1cb5f429bcc1257d2
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-03 19:38:57 +0330

    feat: create login api

--- Backend/modules/v1/auth/routes.js
+++ Backend/modules/v1/auth/routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { register } = require("./controller");
+const { register, login } = require("./controller");
 
 const router = express.Router();
 
 router.route("/register").post(register);
+router.route("/login").post(login);
 
 module.exports = router;
