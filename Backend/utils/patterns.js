919ac969f5eebfcce3314d50782524b1c1b9b7d6
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-03 16:34:36 +0330

    fix: fix namePattern bug

--- Backend/utils/patterns.js
+++ Backend/utils/patterns.js
@@ -1,7 +1,7 @@
 const emailPattern = /^[\w\.\-_]+@[\w]{5,8}\.[a-z]{2,3}$/;
 const phoneNumberPattern = /^(0|\+98)?9\d{9}$/;
 const phoneNumberPrefixPattern = /^(0|\+98)/;
-const namePattern = /^[a-zA-Z]{3}$/;
+const namePattern = /^[a-zA-Z]{3,}$/;
 const usernamePattern = /^[a-z]{1}[a-z0-9]{2,}(_[a-z0-9]+)*?$/;
 
 module.exports = {
