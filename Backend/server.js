210fe6ed3c77f13680ed52888b7866958a6c407b
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-03 16:33:22 +0330

    feat: connect to database

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,9 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const connectToDB = require("./configs/db");
 
 dotenv.config();
+connectToDB();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
