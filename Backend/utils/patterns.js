e2349f633552b9b2bc33d504e43bd50ea3d112e8
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-03 12:51:39 +0330

    feat: add namePattern

--- Backend/utils/patterns.js
+++ Backend/utils/patterns.js
@@ -1,9 +1,11 @@
 const emailPattern = /^[\w\.\-_]+@[\w]{5,8}\.[a-z]{2,3}$/;
 const phoneNumberPattern = /^(0|\+98)?9\d{9}$/;
 const phoneNumberPrefixPattern = /^(0|\+98)/;
+const namePattern = /^[a-zA-Z]{3}$/;
 
 module.exports = {
   emailPattern,
   phoneNumberPattern,
   phoneNumberPrefixPattern,
+  namePattern,
 };
