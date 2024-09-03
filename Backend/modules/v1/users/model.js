645207445fc21c297353acdd0079b2b504ec5518
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-03 13:27:46 +0330

    feat: create users model

---
+++ Backend/modules/v1/users/model.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const roles = require("./../../../utils/constants");
+const {
+  emailPattern,
+  phoneNumberPattern,
+  namePattern,
+  usernamePattern,
+} = require("./../../../utils/patterns");
+
+const schema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      trim: true,
+      minLength: 3,
+      match: namePattern,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      trim: true,
+      minLength: 3,
+      match: namePattern,
+      required: true,
+    },
+    username: {
+      type: String,
+      trim: true,
+      unique: true,
+      minLength: 3,
+      match: usernamePattern,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      match: emailPattern,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      match: phoneNumberPattern,
+      unique: true,
+    },
+    age: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    role: {
+      type: String,
+      trim: true,
+      enum: [roles.MANAGER, roles.ADMIN, roles.USER],
+      default: roles.USER,
+    },
+  },
+  { timestamps: true }
+);
+
+const model = mongoose.models.User || mongoose.model("User", schema);
+
+module.exports = model;
