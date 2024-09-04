d08adbe726b6d9cdf9b154a5a890528c3bd8cfd1
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-04 16:53:32 +0330

    feat: create createRoom api

--- Backend/modules/v1/rooms/routes.js
+++ Backend/modules/v1/rooms/routes.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const authMiddleware = require("./../../../utils/middlewares/authMiddleware");
+const accessLevelMiddleware = require("./../../../utils/middlewares/accessLevelMiddleware");
+const { roles } = require("./../../../utils/constants");
+const { createRoom } = require("./controller");
+const { uploader } = require("../../../utils/uploader");
 
 const router = express.Router();
+
+router
+  .route("/")
+  .post(
+    authMiddleware,
+    accessLevelMiddleware(roles.ADMIN),
+    uploader("public/rooms/images").single("image"),
+    createRoom
+  );
 
 module.exports = router;
