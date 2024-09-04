992b37353b2afeabce42911728a971daaac2f8cd
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-04 16:50:43 +0330

    feat: create createRoom function

--- Backend/modules/v1/rooms/controller.js
+++ Backend/modules/v1/rooms/controller.js
@@ -1,1 +1,100 @@
 const roomModel = require("./model");
+const namespaceModel = require("./../namespaces/model");
+const createRoomValidate = require("./../../../utils/validators/rooms/createRoom");
+const roomImageValidate = require("./../../../utils/validators/rooms/roomImage");
+const fs = require("node:fs");
+const path = require("node:path");
+const { isValidObjectId } = require("mongoose");
+const { defaultUserPhoto } = require("../../../utils/constants");
+
+require("dotenv").config();
+
+const createRoom = async (req, res, next) => {
+  if ("file" in req) {
+    const isValidImage = roomImageValidate(req);
+    if (!isValidImage) {
+      return res.status(422).json(roomImageValidate.errors);
+    }
+  }
+
+  const isValidRequestBody = createRoomValidate(req.body);
+  if (!isValidRequestBody) {
+    if ("file" in req) {
+      fs.unlinkSync(
+        path.join(process.cwd(), "/public/rooms/images", req.file.filename)
+      );
+    }
+    return res.status(422).json(createRoomValidate.errors);
+  }
+
+  const { title, href, namespaceId } = req.body;
+
+  const isValidId = isValidObjectId(namespaceId);
+  if (!isValidId) {
+    return res.status(422).json({ message: "NamespaceId is not valid !!" });
+  }
+
+  try {
+    await checkDBCollectionIndexes(roomModel);
+  } catch (err) {
+    const isRoomExistBefore = await roomModel
+      .findOne({ title, href, namespaceId })
+      .lean();
+    if (isRoomExistBefore) {
+      if ("file" in req) {
+        fs.unlinkSync(
+          path.join(process.cwd(), "public/rooms/images", req.file.filename)
+        );
+      }
+      return res.status(422).json({ message: "Room is already exist !!" });
+    }
+  }
+
+  try {
+    const namespace = await namespaceModel.findOne({ _id: namespaceId });
+    if (!namespace) {
+      if ("file" in req) {
+        fs.unlinkSync(
+          path.join(process.cwd(), "public/rooms/images", req.file.filename)
+        );
+      }
+      return res.status(422).json({ message: "Namespace not found !!" });
+    }
+
+    const image =
+      "file" in req
+        ? `${process.env.BASE_URL}/rooms/images/${req?.file?.filename}`
+        : defaultUserPhoto;
+
+    const newRoom = await roomModel.create({
+      ...req.body,
+      image,
+    });
+    if (!newRoom) {
+      return res.status(500).json({ message: "Create Room failed !!" });
+    }
+
+    const room = await roomModel
+      .findOne({ _id: newRoom._id })
+      .populate("namespaceId", "title href")
+      .select("-__v")
+      .lean();
+
+    room.namespace = room.namespaceId;
+    delete room.namespaceId;
+
+    return res.status(201).json({
+      message: "Room added successfully :))",
+      room,
+    });
+  } catch (err) {
+    if ("file" in req) {
+      fs.unlinkSync(
+        path.join(process.cwd(), "public/rooms/images", req.file.filename)
+      );
+    }
+    next(err);
+  }
+};
+
+module.exports = { createRoom };
