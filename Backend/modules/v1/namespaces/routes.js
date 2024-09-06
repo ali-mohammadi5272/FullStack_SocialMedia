a6a6541569930d2bcb51dbab64c1287dabc69142
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-06 10:58:17 +0330

    feat: create getAll api

--- Backend/modules/v1/namespaces/routes.js
+++ Backend/modules/v1/namespaces/routes.js
@@ -2,12 +2,13 @@ const express = require("express");
 const authMiddleware = require("./../../../utils/middlewares/authMiddleware");
 const accessLevelMiddleware = require("./../../../utils/middlewares/accessLevelMiddleware");
 const { roles } = require("./../../../utils/constants");
-const { createNamespace } = require("./controller");
+const { createNamespace, getAll } = require("./controller");
 
 const router = express.Router();
 
 router
   .route("/")
+  .get(getAll)
   .post(authMiddleware, accessLevelMiddleware(roles.ADMIN), createNamespace);
 
 module.exports = router;
