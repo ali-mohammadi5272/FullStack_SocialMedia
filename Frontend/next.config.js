7113f1c4ba3f6ed2359fc769ab47ed87cdc10155
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-10 16:13:03 +0330

    feat: sass configs

--- Frontend/next.config.js
+++ Frontend/next.config.js
@@ -1,7 +1,14 @@
+const path = require("path");
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  sassOptions: {
+    sassOptions: {
+      includePaths: [path.join(__dirname, "styles")],
+    },
   },
 };
 
