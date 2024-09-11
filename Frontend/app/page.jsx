9f00aaa057dee74900826b1bb891aee66fdff5e5
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-11 09:12:46 +0330

    refactor: change files extensions

---
+++ Frontend/app/page.jsx
@@ -0,0 +1,8 @@
+import styles from "./page.module.scss";
+import HomePage from "@/components/templates/HomePage/HomePage";
+
+const Page = function () {
+  return <HomePage />;
+};
+
+export default Page;
