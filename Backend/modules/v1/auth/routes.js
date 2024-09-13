a0851a630836e2f681d51ea85c6c0cf6dc1ec7b6
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-13 16:18:08 +0330

    feat: create getMe api

--- Backend/modules/v1/auth/routes.js
+++ Backend/modules/v1/auth/routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { register, login, logout } = require("./controller");
+const authMiddleware = require("./../../../utils/middlewares/authMiddleware");
+const { register, login, logout, getMe } = require("./controller");
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").post(logout);
+router.route("/me").get(authMiddleware, getMe);
 
 module.exports = router;
