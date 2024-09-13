6f7a8b8a9367028dbe989c4ae80d05221038d705
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-13 16:18:29 +0330

    feat: create AxiosContext.jsx

---
+++ Frontend/contexts/AxiosContext.jsx
@@ -0,0 +1,33 @@
+"use client";
+import { createContext } from "react";
+import { axiosRequest } from "@/services/axios";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+
+export const AxiosContext = createContext();
+
+const AxiosProvider = ({ children }) => {
+  const router = useRouter();
+
+  axiosRequest.interceptors.response.use(
+    (response) => {
+      toast.success(response.data.message);
+      return response;
+    },
+    (err) => {
+      if (err.response.status === 401) {
+        toast.error(err.response.data.message);
+        router.replace("/login");
+      }
+      return Promise.reject(err);
+    }
+  );
+
+  return (
+    <AxiosContext.Provider value={axiosRequest}>
+      {children}
+    </AxiosContext.Provider>
+  );
+};
+
+export default AxiosProvider;
