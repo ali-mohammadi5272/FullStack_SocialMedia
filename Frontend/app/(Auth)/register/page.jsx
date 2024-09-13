1c4c4db306ef10bbfd4c13d854f524931579f9f3
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-13 21:34:39 +0330

    feat: create RegisterPage

---
+++ Frontend/app/(Auth)/register/page.jsx
@@ -0,0 +1,8 @@
+import RegisterPage from "@/components/templates/RegisterPage/RegisterPage";
+import React from "react";
+
+const Page = () => {
+  return <RegisterPage />;
+};
+
+export default Page;
