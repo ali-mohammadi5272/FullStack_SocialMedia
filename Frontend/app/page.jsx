71c1b5406ab9e8e5e7b0c1a1cf6e141bd3ba0325
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-13 21:35:28 +0330

    feat: add AuthProvider

--- Frontend/app/page.jsx
+++ Frontend/app/page.jsx
@@ -1,8 +1,13 @@
+import AuthProvider from "@/contexts/AuthContext";
 import styles from "./page.module.scss";
 import HomePage from "@/components/templates/HomePage/HomePage";
 
 const Page = function () {
-  return <HomePage />;
+  return (
+    <AuthProvider>
+      <HomePage />
+    </AuthProvider>
+  );
 };
 
 export default Page;
