864f005b5482919e43108e135583a1886b2bf66f
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-13 16:19:09 +0330

    feat: create AuthContext

--- Frontend/contexts/AuthContext.jsx
+++ Frontend/contexts/AuthContext.jsx
@@ -4,7 +4,7 @@ import { useState, createContext } from "react";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({ id: 1, username: "ali" });
+  const [user, setUser] = useState(null);
   const [contact, setContact] = useState(null);
 
   return (
@@ -22,4 +22,3 @@ const AuthProvider = ({ children }) => {
 };
 
 export default AuthProvider;
-A
