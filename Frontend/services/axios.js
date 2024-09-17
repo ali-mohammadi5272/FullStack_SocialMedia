6b72ad8f06bbfa12ce7bd256c1aa4f67cef59773
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-17 15:16:04 +0330

    Revert "refactor: remove logics. Removed logics are in AxiosContext"
    This reverts commit e01a2860a56f86cb1638fa3552daf3ec08e31892.

--- Frontend/services/axios.js
+++ Frontend/services/axios.js
@@ -4,4 +4,29 @@ const axiosRequest = axios.create({
   baseURL: `http://localhost:3000/api/v1`,
 });
 
+axiosRequest.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (err) => {
+    console.log(err);
+    toast.error(err.response.data.message);
+    return Promise.reject(err);
+  }
+);
+
+axiosRequest.interceptors.response.use(
+  (response) => {
+    toast.success(response.data.message);
+    return response;
+  },
+  (err) => {
+    if (err.code === "ERR_NETWORK") {
+      toast.error(err.message);
+    }
+
+    return Promise.reject(err);
+  }
+);
+
 export { axiosRequest };
