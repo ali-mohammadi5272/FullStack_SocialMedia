2c5a564dd97017ae083023365261c27b6939c32c
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-17 15:16:36 +0330

    Revert "refactor: axios service logic are in AxiosContext"
    This reverts commit 1461e10b4fd8d3e17529b34e6af8c3d77bc1c818.

--- Frontend/contexts/AxiosContext.jsx
+++ Frontend/contexts/AxiosContext.jsx
@@ -9,45 +9,15 @@ export const AxiosContext = createContext();
 const AxiosProvider = ({ children }) => {
   const router = useRouter();
 
-  axiosRequest.interceptors.request.use(
-    (config) => {
-      return config;
-    },
-    (err) => {
-      toast.error(err.response.data.message);
-      return Promise.reject(err);
-    }
-  );
-
   axiosRequest.interceptors.response.use(
     (response) => {
-      toast.success(response.data.message);
       return response;
     },
     (err) => {
-      if (err.code === "ERR_NETWORK") {
-        toast.error(err.message);
-      }
-
       if (err.response.status === 401) {
         toast.error(err.response.data.message);
         router.replace("/login");
       }
-
-      const isErrorAnArray =
-        typeof err.response.data === "object" &&
-        "length" in err.response.data &&
-        err.response.data.length;
-
-      if (isErrorAnArray) {
-        err.response.data.forEach((error) => {
-          toast.error(error.message);
-        });
-      } else {
-        toast.error(err.response.data.message);
-      }
-      console.log(err);
-      return Promise.reject(err);
     }
   );
 
