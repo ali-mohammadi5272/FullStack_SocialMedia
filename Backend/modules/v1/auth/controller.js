ebc159988d6e31194da3c7f4143520a46f430977
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-17 19:16:43 +0330

    feat: add refreshToken and accessToken to register response

--- Backend/modules/v1/auth/controller.js
+++ Backend/modules/v1/auth/controller.js
@@ -63,6 +63,8 @@ const register = async (req, res, next) => {
     return res.status(201).json({
       message: "User registered successfully :))",
       user: newUserObject,
+      accessToken,
+      refreshToken,
     });
   } catch (err) {
     return next(err);
