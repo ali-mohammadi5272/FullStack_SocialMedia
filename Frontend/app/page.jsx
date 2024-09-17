c25f7148bf476ecee1a34ebdbd5edd99ba63f8d3
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-17 19:26:25 +0330

    feat: authenticate user in HomePage

--- Frontend/app/page.jsx
+++ Frontend/app/page.jsx
@@ -1,8 +1,15 @@
 import AuthProvider from "@/contexts/AuthContext";
 import styles from "./page.module.scss";
 import HomePage from "@/components/templates/HomePage/HomePage";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const Page = function () {
+  const refreshToken = cookies().get("refreshToken").value;
+  if (!refreshToken) {
+    redirect("/login");
+  }
+
   return (
     <AuthProvider>
       <HomePage />
