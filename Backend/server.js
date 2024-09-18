b7fc03da15209a6a30724b06f51e96c50eddc5a4
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-18 08:30:17 +0330

    feat: add socket.io configs

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,17 @@
 const httpServer = require("./app");
 const dotenv = require("dotenv");
 const connectToDB = require("./configs/db");
+const socketIo = require("socket.io");
+const socketIoInit = require("./socket.io/socket.io");
 
 dotenv.config();
 connectToDB();
+
+const io = new socketIo.Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
 
 httpServer.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
