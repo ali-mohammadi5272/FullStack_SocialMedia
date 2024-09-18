07df00ca8d3a3bdf83ed81058c07ddce747244c0
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-18 09:30:59 +0330

    feat: component is dynamic

--- Frontend/components/modules/ChatHeader/ChatHeader.jsx
+++ Frontend/components/modules/ChatHeader/ChatHeader.jsx
@@ -1,10 +1,17 @@
+"use client";
 import ChatProfile from "../ChatProfile/ChatProfile";
 import styles from "./chatHeader.module.scss";
+import { useContext } from "react";
+import { AuthContext } from "@/contexts/AuthProvider";
 
 const ChatHeader = () => {
+  const { contact } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
-      <ChatProfile username="Username" />
+      {contact && (
+        <ChatProfile username={contact.username} isTyping={contact.isTyping} />
+      )}
     </header>
   );
 };
