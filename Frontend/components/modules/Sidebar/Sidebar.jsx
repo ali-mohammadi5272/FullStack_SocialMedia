2c68bceebf49cbd50c4de663eaa346dc8b2ac3bc
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-18 09:29:43 +0330

    feat: add ChatProfilesContainer to Sidebar Component

--- Frontend/components/modules/Sidebar/Sidebar.jsx
+++ Frontend/components/modules/Sidebar/Sidebar.jsx
@@ -1,7 +1,12 @@
+import ChatProfilesContainer from "../ChatProfilesContainer/ChatProfilesContainer";
 import styles from "./sidebar.module.scss";
 
 const Sidebar = () => {
-  return <aside className={styles.aside}>Aside</aside>;
+  return (
+    <aside className={styles.aside}>
+      <ChatProfilesContainer />
+    </aside>
+  );
 };
 
 export default Sidebar;
