11332a1469e8f498bbb27c8424708c8d7c86bfc2
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-20 20:14:46 +0330

    refactor: use context instead props

--- Frontend/components/modules/ChatScreen/ChatScreen.jsx
+++ Frontend/components/modules/ChatScreen/ChatScreen.jsx
@@ -1,18 +1,19 @@
+"use client";
 import ChatHeader from "../ChatHeader/ChatHeader";
 import Message from "../Message/Message";
 import styles from "./chatScreen.module.scss";
+import { useContext } from "react";
+import { AuthContext } from "@/contexts/AuthProvider";
 
-const ChatScreen = ({ messages = [], userId = "" }) => {
+const ChatScreen = () => {
+  const { chatMessages } = useContext(AuthContext);
+
   return (
     <section className={styles.chatScreen}>
       <ChatHeader />
       <section className={styles.chatSection}>
-        {messages.map((message) => (
-          <Message
-            own={userId === message.creatorId}
-            key={message._id}
-            {...message}
-          />
+        {chatMessages.map((message) => (
+          <Message key={message._id} {...message} />
         ))}
       </section>
     </section>
