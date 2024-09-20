095f7eac2881fd5685ea4f65c2eddf1e45e819f4
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-20 14:50:14 +0330

    feat: create sendMessagesToRooms function

--- Backend/socket.io/socket.controller.js
+++ Backend/socket.io/socket.controller.js
@@ -23,9 +23,36 @@ const joinRoom = (socket, rooms) => {
   });
 };
 
+const sendMessagesToRooms = async (io, rooms) => {
+  const convertedRoom = [
+    `${rooms[0]}___${rooms[1]}`,
+    `${rooms[1]}___${rooms[0]}`,
+  ];
+
+  try {
+    const messages = await messageModel
+      .find({
+        $or: [
+          { creatorId: rooms[0], receiverId: rooms[1] },
+          { creatorId: rooms[1], receiverId: rooms[0] },
+        ],
+      })
+      .select("-__v -password")
+      .lean();
+
+    io.of("/chats")
+      .in(convertedRoom[0])
+      .in(convertedRoom[1])
+      .emit("chatMessages", messages);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const joinRoomHandler = async (io, socket) => {
   socket.on("joinRoom", (rooms) => {
     joinRoom(socket, rooms);
+    sendMessagesToRooms(io, rooms);
   });
 };
 
