69a67d2616d285f774128d59e13222f2facb59bb
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-23 07:42:13 +0330

    refactor: change socket.io connection event

--- Backend/socket.io/socket.io.js
+++ Backend/socket.io/socket.io.js
@@ -2,7 +2,7 @@ const { sendUsers, joinRoomHandler } = require("./socket.controller");
 
 const socketIoInit = (io) => {
   io.on("connection", async (socket) => {
-    sendUsers(socket);
+    await sendUsers(socket);
   });
   io.of("/chats").on("connection", (socket) => {
     joinRoomHandler(io, socket);
