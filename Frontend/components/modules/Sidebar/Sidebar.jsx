3b2addce5c3d129310bf6f049e395056c5cb39f3
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-09-30 13:57:08 +0330

    feat: add OffCanvasMenu to Sidebar

--- Frontend/components/modules/Sidebar/Sidebar.jsx
+++ Frontend/components/modules/Sidebar/Sidebar.jsx
@@ -1,11 +1,40 @@
+"use client";
+import { useContext } from "react";
+import { AuthContext } from "@/contexts/AuthProvider";
 import ChatProfilesContainer from "../ChatProfilesContainer/ChatProfilesContainer";
 import styles from "./sidebar.module.scss";
+import FontAwesomeIcon from "../FontAwesomeIcon/FontAwesomeIcon";
 
 const Sidebar = () => {
+  const { showSideBar, setShowSideBar } = useContext(AuthContext);
+
+  const showSideBarHandler = () => setShowSideBar((prev) => !prev);
+  const offCanvasMenuClick = () => setShowSideBar(false);
+
   return (
-    <aside className={styles.aside}>
-      <ChatProfilesContainer />
-    </aside>
+    <>
+      <aside
+        className={`${styles.aside} ${showSideBar ? styles.aside_active : ""}`}
+      >
+        <header className={styles.aside__header}>
+          <button
+            onClick={showSideBarHandler}
+            className={styles.aside__hideSideBarIconBtn}
+          >
+            <FontAwesomeIcon icon="faXmark" />
+          </button>
+        </header>
+        <ChatProfilesContainer />
+      </aside>
+      {showSideBar && (
+        <div
+          onClick={offCanvasMenuClick}
+          className={`${styles.blurEffect} ${
+            showSideBar ? styles.blurEffect_active : ""
+          }`}
+        ></div>
+      )}
+    </>
   );
 };
 
